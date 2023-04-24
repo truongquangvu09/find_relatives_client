335ce982cc249f151e787f106f54bb00ad8293d6
VinhTocDo <tongvinh.98.1906@gmail.com>
2023-04-24 19:32:58 +0700

    taskngay18thang4

--- src/utils/sidebar/index.js
+++ src/utils/sidebar/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return <div>
+    <div class="widget-block">
+		<h3 class="widget-title">Liên hệ</h3>
+		<ul class="featured-contact">
+			<li class="contact-phone"><a href="tel:02862647777">(028) 6264 7777</a></li>
+			<li class="contact-mail">Hòm thư 005 - Bưu điện trung tâm Sài Gòn, Tp. Hồ Chí Minh</li>
+			<li class="contact-email">Email: <a href="/cdn-cgi/l/email-protection#03776a6e6d64766c6a776b626d436b627a6f666d776a666d642d756d"><span class="__cf_email__" data-cfemail="e2968b8f8c85978d8b968a838ca28a839b8e878c968b878c85cc948c">[email&#160;protected]</span></a></li>
+		</ul>
+	</div>
+
+	<div class="widget-block">
+		<h3 class="widget-title">Zalo Official Account</h3>
+		<div class="zalo-follow-button" data-oaid="2629363586912501706" data-cover="no" data-article="3" data-width="300" data-height="442"></div>
+	</div>
+  </div>;
 }
 
 export default Sidebar;

25c72bf87d6d510b596c2a71748f1acd44a9ae65
unknown <thienpro14990011@gmail.com>
2023-05-08 19:42:57 +0700

    update_search_account

--- src/admin/page/AccountManage/index.js
+++ src/admin/page/AccountManage/index.js
@@ -1,7 +1,77 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+// import styles from "./detail.module.css"
 
 function AccountManage() {
-  return <div>AccountManage</div>;
+  const columns = [
+    { field: 'id', headerName: 'Number', width: 90 },
+    {
+      field: 'fullName',
+      headerName: 'Họ tên tài khoản',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'account',
+      headerName: 'Tên tài khoản',
+      editable: true,
+      width: 160,
+
+    },
+    {
+      field: 'password',
+      headerName: 'Mật khẩu',
+      editable: true,
+      width: 150,
+
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      width: 180,
+      editable: true,
+    },
+    {
+      field: 'phone',
+      headerName: 'Số điện thoại',
+      width: 120,
+      editable: true,
+    },
+    {
+      field: 'IDcitizen',
+      headerName: 'Căn cước công dân',
+      width: 150,
+      editable: true,
+    },
+
+  ];
+
+  const rows = [
+    { id: 1,  fullName: 'Le Van Thien', account: 'Thien140901', password:123456, email: 'thien@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
+    { id: 2,  fullName: 'Nguyen Van Tung', account: 'Thien140901', password:123456, email: 'tungito@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
+    { id: 3,  fullName: 'Truong Quang Vu', account: 'Thien140901', password:123456, email: 'quangvu@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
+    { id: 4,  fullName: 'Le Thi Hoa', account: 'Thien140901', password:123456, email: 'hoahoa@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
+    { id: 5,  fullName: 'Le Yen Oanh', account: 'Thien140901', password:123456, email: 'oanhleo@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
+    { id: 6,  fullName: 'Hoang Dinh', account: 'Thien140901', password:123456, email: 'Dinha@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
+    { id: 7,  fullName: 'Hoang Yen', account: 'Thien140901', password:123456, email: 'yen@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
+  ];
+
+  return (
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+      />
+    </Box>
+  )
 }
 
 export default AccountManage;
--- src/admin/page/SearchRegistrationManage/index.js
+++ src/admin/page/SearchRegistrationManage/index.js
@@ -5,6 +5,9 @@ import styles from './Registration.module.scss';
 import classNames from 'classnames/bind';
 import Detail_Search from './detail_search/detail';
 import Modal from '@mui/material/Modal';
+import { Button } from '@mui/material';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+
 const cx = classNames.bind(styles);
 
 function SearchRegistrationManage() {
@@ -22,74 +25,61 @@ function SearchRegistrationManage() {
     bgcolor: 'background.paper',
     boxShadow: 24,
     p: 4,
-    width: '80%',
-    height: '70%',
+    width: '55%',
+    height: '100%',
     borderRadius: '20px',
-    
+
   };
   const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'id', headerName: 'MS', width: 100 },
     {
-      field: 'fullName',
-      headerName: 'Tên người đăng kí',
-      width: 150,
+      field: 'title',
+      headerName: 'Tiêu đề',
+      width: 300,
       editable: true,
     },
     {
-      field: 'sex',
-      headerName: 'Giới tính',
-      width: 150,
+      field: 'status',
+      headerName: 'Trạng thái',
+      width: 300,
       editable: true,
     },
     {
-      field: 'age',
-      headerName: 'Năm sinh',
-      width: 110,
+      field: 'button',
+      headerName: '',
+      width: 100,
       editable: true,
-    },
-    {
-      field: 'address',
-      headerName: 'Địa chỉ',
-      editable: true,
-      width: 160,
-
-    },
-    {
-      field: 'email',
-      headerName: 'Email',
-      width: 180,
-      editable: true,
-    },
-    {
-      field: 'phone',
-      headerName: 'Số điện thoại',
-      width: 120,
-      editable: true,
-    },
-    {
-      field: 'IDcitizen',
-      headerName: 'Căn cước công dân',
-      width: 150,
-      editable: true,
+      renderCell: (cellValues) => {
+        return (
+          <Button
+            variant="contained"
+            color="primary"
+            
+          >
+            <DeleteForeverIcon></DeleteForeverIcon>
+          </Button>
+        );
+      }
     },
 
   ];
 
   const rows = [
-    { id: 1, sex: 'Nam', fullName: 'Le Van Thien', age: 2001, address: 'Que son, Quang nam', email: 'thien@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
-    { id: 2, sex: 'Nam', fullName: 'Nguyen Van Tung', age: 2001, address: 'Que son, Quang nam', email: 'tungito@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
-    { id: 3, sex: 'Nam', fullName: 'Truong Quang Vu', age: 2001, address: 'Que son, Quang nam', email: 'quangvu@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
-    { id: 4, sex: 'Nữ', fullName: 'Le Thi Hoa', age: 2001, address: 'Que son, Quang nam', email: 'hoahoa@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
-    { id: 5, sex: 'Nữ', fullName: 'Le Yen Oanh', age: 2001, address: 'Que son, Quang nam', email: 'oanhleo@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
-    { id: 6, sex: 'Nam', fullName: 'Hoang Dinh', age: 2001, address: 'Que son, Quang nam', email: 'Dinha@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
-    { id: 7, sex: 'Nữ', fullName: 'Hoang Yen', age: 2001, address: 'Que son, Quang nam', email: 'yen@gmail.com', phone: '08438438', IDcitizen: 34534532524 },
+    { id: 1, title: 'Đơn đăng kí 1', status: null, },
+    { id: 2, title: 'Đơn đăng kí 2', status: null, },
+    { id: 3, title: 'Đơn đăng kí 3', status: null, },
+    { id: 4, title: 'Đơn đăng kí 4', status: null, },
+    { id: 5, title: 'Đơn đăng kí 5', status: null, },
+    { id: 6, title: 'Đơn đăng kí 6', status: null, },
+    { id: 7, title: 'Đơn đăng kí 7', status: null, },
   ];
+
   return (
     <div>
       <h1 className={cx('header')}>Quản lý đơn đăng kí</h1>
       <div className={cx('header-search')}>
         <h3 className={cx('left')}>Tất cả đơn đăng kí</h3>
-        <input placeholder=" Nhập người cần tìm"></input>
+        <input placeholder=" Nhập đơn cần tìm"></input>
       </div>
       <Box sx={{ height: 420, width: '100%' }}>
         <DataGrid
@@ -104,6 +94,7 @@ function SearchRegistrationManage() {
             },
           }}
         />
+
       </Box>
       <Modal
         open={open}
@@ -112,7 +103,7 @@ function SearchRegistrationManage() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <h1 className={cx('header-detail')}>Thông tin người thất lạc</h1>
+          <h1 className={cx('header-detail')}>Đơn đăng kí 1</h1>
           <Detail_Search />
         </Box>
       </Modal>
